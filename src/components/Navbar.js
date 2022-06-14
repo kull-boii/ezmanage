176375bb0d190d37a1f56e183e8f5468c543433b
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-14 12:45:10 +0530

    logout and online implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
-export const Navbar = () => {
+export default function Navbar() {
+  const { logout, isPending } = useLogout();
+
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <ul>
         <li className="logo">
-          <img src={Temple} alt="ezmanage logo" />
-          <span>EzManage</span>
+          <img src={Temple} alt="dojo logo" />
+          <span>The Dojo</span>
         </li>
+
         <li>
           <Link to="/login">Login</Link>
         </li>
@@ -17,9 +21,18 @@ export const Navbar = () => {
           <Link to="/signup">Signup</Link>
         </li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button className="btn" disabled>
+              Logging out...
+            </button>
+          )}
         </li>
       </ul>
-    </div>
+    </nav>
   );
-};
+}
